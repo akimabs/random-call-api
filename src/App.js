cb149201360c74714e55773bb0e065a95d766e76
akimabs <kimuwhehe@gmail.com>
2021-10-03 04:47:34 +0700

    add card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import { Header } from "./components/";
+import { Header, Card } from "./components";
 
 function App() {
   return (
     <div className="mx-0 xl:mx-40 font-body">
       <Header />
+      <Card />
     </div>
   );
 }
---
+++ src/components/card.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import github from "../assets/github.svg";
+
+export default function Card(props) {
+  return (
+    <div className="mx-4 my-8 rounded-md p-4 border-2 border-grey-300 md:px-4">
+      <h1 className="text-xl py-2">Random Activity</h1>
+      {/* <h1 className="text-2xl font-medium pb-6">{response}</h1> */}
+    </div>
+  );
+}
