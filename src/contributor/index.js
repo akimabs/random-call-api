9b0b73478ecd7bd90c68ba4783c3d16c897eafaf
hendraaagil <hendraaagil@gmail.com>
2021-10-03 14:46:19 +0700

    feat: add lorem picsum

--- src/contributor/index.js
+++ src/contributor/index.js
@@ -3,6 +3,7 @@ import RandomCatImage from "./random-cat-image";
 import RandomTechImages from './random-tech-images';
 import RandomPokemonImage from './random-pokemon-image';
 import RandomUnsplashImage from './random-unsplash-image';
+import RandomPicsumImage from './random-picsum-image';
 
 export const data_contributor = [
   RandomFoxImage,
@@ -10,4 +11,5 @@ export const data_contributor = [
   RandomTechImages,
   RandomPokemonImage,
   RandomUnsplashImage,
+  RandomPicsumImage,
 ]
