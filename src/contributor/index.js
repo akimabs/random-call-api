9d257caef1d56e86c16fc5626833570972b04dac
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2021-10-04 17:32:17 +0700

    feat: fix import random quotes api to index.js

--- src/contributor/index.js
+++ src/contributor/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import RandomFoxImage from './random-fox-image';
 import RandomCatImage from './random-cat-image';
 import RandomTechImages from './random-tech-images';
@@ -12,8 +11,9 @@ import RandomAnswerImage from './random-answer-image';
 import RandomDogImage from './random-dog-image';
 import RandomMemeImage from './random-meme-image';
 import RandomUserProfile from './random-user-profile';
+import RandomKanji from './random-kanji';
 import RandomPixelMonstersImage from './random-pixel-monsters-image';
-import RandomAiArt from './random-ai-art';
+import RandomAiArtImage from './random-ai-art';
 import RandomQuotesApi from './random-quote';
 
 export const data_contributor = [
@@ -30,43 +30,8 @@ export const data_contributor = [
 	RandomAnswerImage,
 	RandomDogImage,
 	RandomMemeImage,
+	RandomKanji,
 	RandomPixelMonstersImage,
-	RandomAiArt,
+	RandomAiArtImage,
 	RandomQuotesApi,
-=======
-import RandomFoxImage from "./random-fox-image";
-import RandomCatImage from "./random-cat-image";
-import RandomTechImages from "./random-tech-images";
-import RandomPokemonImage from "./random-pokemon-image";
-import RandomUnsplashImage from "./random-unsplash-image";
-import RandomPicsumImage from "./random-picsum-image";
-import RandomGrayscaleImage from "./random-grayscale-image";
-import RandomImageUnsplash from "./random-image-unsplash";
-import RandomLoremPixelImage from "./random-lorem-pixel-image";
-import RandomAnswerImage from "./random-answer-image";
-import RandomDogImage from "./random-dog-image";
-import RandomMemeImage from "./random-meme-image";
-import RandomUserProfile from "./random-user-profile";
-import RandomKanji from "./random-kanji";
-import RandomPixelMonstersImage from "./random-pixel-monsters-image";
-import RandomAiArtImage from "./random-ai-art";
-
-export const data_contributor = [
-  RandomFoxImage,
-  RandomCatImage,
-  RandomTechImages,
-  RandomPokemonImage,
-  RandomUnsplashImage,
-  RandomPicsumImage,
-  RandomUserProfile,
-  RandomGrayscaleImage,
-  RandomImageUnsplash,
-  RandomLoremPixelImage,
-  RandomAnswerImage,
-  RandomDogImage,
-  RandomMemeImage,
-  RandomKanji,
-  RandomPixelMonstersImage,
-  RandomAiArtImage,
->>>>>>> 004a15a68464d5652a71e682616e17aa13173e90
 ];
