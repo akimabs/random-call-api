bd537497721b4db8f58b3035b8323f5d715848e0
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2021-10-04 12:45:13 +0700

    feat: random quote api ( fix index.js )

--- src/contributor/index.js
+++ src/contributor/index.js
@@ -1,33 +1,35 @@
-import RandomFoxImage from "./random-fox-image";
-import RandomCatImage from "./random-cat-image";
-import RandomTechImages from "./random-tech-images";
-import RandomPokemonImage from "./random-pokemon-image";
-import RandomUnsplashImage from "./random-unsplash-image";
-import RandomPicsumImage from "./random-picsum-image";
-import RandomGrayscaleImage from "./random-grayscale-image";
-import RandomImageUnsplash from "./random-image-unsplash";
-import RandomLoremPixelImage from "./random-lorem-pixel-image";
-import RandomAnswerImage from "./random-answer-image";
-import RandomDogImage from "./random-dog-image";
-import RandomMemeImage from "./random-meme-image";
-import RandomUserProfile from "./random-user-profile";
-import RandomPixelMonstersImage from "./random-pixel-monsters-image";
-import RandomAiArt from "./random-ai-art";
+import RandomFoxImage from './random-fox-image';
+import RandomCatImage from './random-cat-image';
+import RandomTechImages from './random-tech-images';
+import RandomPokemonImage from './random-pokemon-image';
+import RandomUnsplashImage from './random-unsplash-image';
+import RandomPicsumImage from './random-picsum-image';
+import RandomGrayscaleImage from './random-grayscale-image';
+import RandomImageUnsplash from './random-image-unsplash';
+import RandomLoremPixelImage from './random-lorem-pixel-image';
+import RandomAnswerImage from './random-answer-image';
+import RandomDogImage from './random-dog-image';
+import RandomMemeImage from './random-meme-image';
+import RandomUserProfile from './random-user-profile';
+import RandomPixelMonstersImage from './random-pixel-monsters-image';
+import RandomAiArt from './random-ai-art';
+import RandomQuotesApi from './random-quote';
 
 export const data_contributor = [
-  RandomFoxImage,
-  RandomCatImage,
-  RandomTechImages,
-  RandomPokemonImage,
-  RandomUnsplashImage,
-  RandomPicsumImage,
-  RandomUserProfile,
-  RandomGrayscaleImage,
-  RandomImageUnsplash,
-  RandomLoremPixelImage,
-  RandomAnswerImage,
-  RandomDogImage,
-  RandomMemeImage,
-  RandomPixelMonstersImage,
-  RandomAiArt,
+	RandomFoxImage,
+	RandomCatImage,
+	RandomTechImages,
+	RandomPokemonImage,
+	RandomUnsplashImage,
+	RandomPicsumImage,
+	RandomUserProfile,
+	RandomGrayscaleImage,
+	RandomImageUnsplash,
+	RandomLoremPixelImage,
+	RandomAnswerImage,
+	RandomDogImage,
+	RandomMemeImage,
+	RandomPixelMonstersImage,
+	RandomAiArt,
+	RandomQuotesApi,
 ];
