4c94acab66ab044be47570e5ab013692ae45b878
mfauzanamzar <mfauzanamza@gmail.com>
2021-10-27 18:35:52 +0800

    add random koala image

---
+++ src/contributor/random-koala-image.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import { Card } from '../components'
+import axios from 'axios'
+
+const RandomKoalaImage = () => {
+    const [data, setData] = useState()
+    const BASE_URL = 'https://some-random-api.ml/img/koala'
+
+    useEffect(() => {
+        axios
+            .get(BASE_URL)
+            .then(res => setData(res.data))
+            .catch(err => console.log(err))
+    }, [])
+
+    console.log(data)
+    return (
+        <Card data={{
+            username: "mfauzanamzar",
+            avatar: "https://avatars.githubusercontent.com/u/79294189?v=4",
+            apiUrl: "https://some-random-api.ml/img/koala",
+            apiName: "RandomKoala",
+            apiDescription: "Random pictures of koala",
+        }}>
+            {data && (
+                <img
+                    className="rounded-sm h-200 w-200 pt-2"
+                    src={data.link}
+                    alt="koalaimage"
+                />
+            )}
+
+        </Card>
+    )
+}
+
+export default RandomKoalaImage
