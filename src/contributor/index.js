a40fd25a96ad2cfb4825552cc7fc95272c791028
Bayu Erich <rich.bayue@gmail.com>
2022-10-09 18:17:19 +0700

    [add] wink

--- src/contributor/index.js
+++ src/contributor/index.js
@@ -1,25 +1,27 @@
-import RandomFoxImage from "./random-fox-image";
+import RandomAiArtImage from "./random-ai-art";
+import RandomAnimeWink from "./random-anime-wink";
+import RandomAnswerImage from "./random-answer-image";
 import RandomCatImage from "./random-cat-image";
-import RandomTechImages from "./random-tech-images";
-import RandomPokemonImage from "./random-pokemon-image";
-import RandomUnsplashImage from "./random-unsplash-image";
-import RandomPicsumImage from "./random-picsum-image";
+import RandomDogImage from "./random-dog-image";
+import RandomFoodImage from "./random-food-image";
+import RandomFoxImage from "./random-fox-image";
 import RandomGrayscaleImage from "./random-grayscale-image";
 import RandomImageUnsplash from "./random-image-unsplash";
-import RandomLoremPixelImage from "./random-lorem-pixel-image";
-import RandomAnswerImage from "./random-answer-image";
-import RandomDogImage from "./random-dog-image";
-import RandomMemeImage from "./random-meme-image";
-import RandomUserProfile from "./random-user-profile";
 import RandomKanji from "./random-kanji";
+import RandomLoremPixelImage from "./random-lorem-pixel-image";
+import RandomMemeImage from "./random-meme-image";
+import RandomPicsumImage from "./random-picsum-image";
 import RandomPixelMonstersImage from "./random-pixel-monsters-image";
-import RandomAiArtImage from "./random-ai-art";
+import RandomPokemonImage from "./random-pokemon-image";
 import RandomQuotesApi from "./random-quote";
 import RandomRobotImage from "./random-robot-image";
-import RandomFoodImage from "./random-food-image";
+import RandomTechImages from "./random-tech-images";
+import RandomUnsplashImage from "./random-unsplash-image";
+import RandomUserProfile from "./random-user-profile";
 
 export const data_contributor = [
   RandomFoxImage,
+  RandomAnimeWink,
   RandomCatImage,
   RandomTechImages,
   RandomPokemonImage,
@@ -37,5 +39,5 @@ export const data_contributor = [
   RandomAiArtImage,
   RandomQuotesApi,
   RandomRobotImage,
-  RandomFoodImage
+  RandomFoodImage,
 ];
